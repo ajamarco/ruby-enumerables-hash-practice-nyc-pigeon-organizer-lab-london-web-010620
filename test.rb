1a6aff649d17e6d5fc4989bd3f482f02974e8f61
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-19 15:24:43 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -38,8 +38,8 @@ end
 #==================================================================
 
 return_hash = pigeon_names
-pigeon_names.each_key do |key|
-  puts "o nome do passaro é #{key}"
+pigeon_names.each_key do |bird_name|
+  puts "o nome do passaro é #{bird_name}"
   pigeon_data.each do |key, value|
     puts "dentro do data. A key é #{key} e o value é #{value}"
   end 
