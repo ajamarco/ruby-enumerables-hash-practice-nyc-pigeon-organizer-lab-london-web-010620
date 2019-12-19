b0b4db0183a4ed04897cfdfd2791fa06886afc0a
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-19 16:03:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+def get_pigeon_names(birds_hash)
+  names_array = [] #create a new array to hold the birds' names
+  birds_hash.each do |key, value| #iterate through the parent hash
+    birds_hash[key].each do |key2, value2| #iterate through the hash within
+      #add into the array the bird name inside this hash
+      names_array.push(value2[0]) 
+    end
+  end
+  names_array.uniq! #drop all duplicated names 
+  returned_hash = {} #create a new hash to return 
+  
+  #iterate through the array and create a new hash with the names to return
+  names_array.length.times do |index| 
+    returned_hash[names_array[index]] = {}
+  end
+  returned_hash
+end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  return_has
 end
